155785c245ae4c30fcc30f324371061e7ecd930c
pengrey <rodrigoflima.me@gmail.com>
2025-08-26 22:05:58 +0000

    init bof (working better)

--- BOF/src/bof.c
+++ BOF/src/bof.c
@@ -1,15 +1,12 @@
 #include <windows.h>
 #include "beacon.h"
 
-DECLSPEC_IMPORT BOOL ADVAPI32$GetUserNameA(LPSTR lpBuffer, LPDWORD pcbBuffer);
-DECLSPEC_IMPORT DWORD KERNEL32$GetLastError();
-
 void go(char* args, int argc) {
     CHAR  User[MAX_PATH + 1] = { 0 };
     DWORD Size               = MAX_PATH;
 
-    if (!ADVAPI32$GetUserNameA(User, &Size)) {
-        BeaconPrintf(CALLBACK_ERROR, "[!] GetUserNameA Failed with Error: %ld", KERNEL32$GetLastError());
+    if (!GetUserNameA(User, &Size)) {
+        BeaconPrintf(CALLBACK_ERROR, "[!] GetUserNameA Failed with Error: %ld", GetLastError());
         return;
     }
 
