1c03a82426af7aaaf47dcf5979085bc8d19786c2
pengrey <rodrigoflima.me@gmail.com>
2025-08-26 20:01:04 +0000

    init bof (working)

--- BOF/src/bof.cc
+++ BOF/src/bof.cc
@@ -1,5 +1,23 @@
 #include <beacon.h>
+#include <macros.h>
+
+extern "C" {
+    DFR(ADVAPI32, GetUserNameA);
+    #define GetUserNameA ADVAPI32$GetUserNameA
+
+    DFR(KERNEL32, GetLastError);
+    #define GetLastError KERNEL32$GetLastError
+}
 
 void go(char* args, int argc) {
-    BeaconPrintf(CALLBACK_OUTPUT, "Hello from Beacon Object File: %s [%d bytes]", args, argc);
+    CHAR  User[MAX_PATH + 1] = { 0 };
+    DWORD Size               = MAX_PATH;
+
+    if (!GetUserNameA(User, &Size)) {
+        BeaconPrintf(CALLBACK_ERROR, "[!] GetUserNameA Failed with Error: %ld", KERNEL32$GetLastError());
+        return;
+    }
+
+    BeaconPrintf(CALLBACK_OUTPUT, "Username: %s", User);
 }
+
